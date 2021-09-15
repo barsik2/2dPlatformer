d3f031d1f3e6d1634bbf679d5a2f45df078abb72
Bovaev Arslan <arslan.bovaeff@gmail.com>
2021-09-15 15:18:20 +0300

    version 0.0.2

--- first/src/Todo/ToDoList.js
+++ first/src/Todo/ToDoList.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import ToDoItem from './ToDoItem'
 
-const styles = {
-    ul: {
-        listStyle: 'none',
-        margin: 0,
-        padding: 0
-    }
-}
 
 export default function ToDoList(props) {
     return(
-        <ul style = {styles.ul}>
+        <ul className = 'styles'>
        { props.todos.map(todo => {
            return <ToDoItem todo = {todo}/>
        }) }
